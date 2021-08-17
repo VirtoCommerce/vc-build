a1e225f5470d180beb30294648473de4ae67aff0
Alexander Morogov <asmorogov@gmail.com>
2021-08-17 15:01:37 +0200

    VCI-128: Fix codesmells

--- Configuration.cs
+++ Configuration.cs
@@ -4,8 +4,8 @@ using Nuke.Common.Tooling;
 [TypeConverter(typeof(TypeConverter<Configuration>))]
 public class Configuration : Enumeration
 {
-    public static Configuration Debug = new Configuration { Value = nameof(Debug) };
-    public static Configuration Release = new Configuration { Value = nameof(Release) };
+    public readonly static Configuration Debug = new Configuration { Value = nameof(Debug) };
+    public readonly static Configuration Release = new Configuration { Value = nameof(Release) };
 
     public static implicit operator string(Configuration configuration)
     {
