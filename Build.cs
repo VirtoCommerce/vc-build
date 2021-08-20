194ebd0c826b627719d1b1d540e841ced2407eab
Alexander Morogov <asmorogov@gmail.com>
2021-08-20 16:46:18 +0200

    VCI-128: Fix build project search

--- Build.cs
+++ Build.cs
@@ -220,7 +220,7 @@ namespace VirtoCommerce.Build
         protected static AbsolutePath SamplesDirectory => RootDirectory / "samples";
 
         protected AbsolutePath ModulesLocalDirectory => ArtifactsDirectory / ModulesJsonDirectoryName;
-        protected Project WebProject => Solution?.AllProjects.FirstOrDefault(x => x.Name.EndsWith(".Web") && !x.Path.ToString().Contains("samples") || x.Name.EndsWith("VirtoCommerce.Storefront"));
+        protected Project WebProject => Solution?.AllProjects.FirstOrDefault(x => x.Name.EndsWith(".Web") && !x.Path.ToString().Contains("samples") || x.Name.EndsWith("VirtoCommerce.Storefront") || x.Name.EndsWith("_build"));
         protected AbsolutePath ModuleManifestFile => WebProject.Directory / "module.manifest";
         protected AbsolutePath ModuleIgnoreFile => RootDirectory / "module.ignore";
 
