91da2fb2f62f208d7e5173eefc24d4a56c038134
Alexander Morogov <asmorogov@gmail.com>
2021-09-22 08:45:20 +0200

    VCI-156: Add ability to update only platform

--- PlatformTools/Build.PackageManager.cs
+++ PlatformTools/Build.PackageManager.cs
@@ -28,7 +28,7 @@ namespace VirtoCommerce.Build
         public static bool SkipDependencySolving { get; set; }
 
         [Parameter("Install the platform", Name = "Platform")]
-        public static bool InstallPlatformParam { get; set; }
+        public static bool PlatformParameter { get; set; }
 
         public Target Init => _ => _
             .Executes(async () =>
@@ -47,7 +47,7 @@ namespace VirtoCommerce.Build
                 var localModuleCatalog = LocalModuleCatalog.GetCatalog(GetDiscoveryPath(), ProbingPath);
                 var externalModuleCatalog = ExtModuleCatalog.GetCatalog(GitHubToken, localModuleCatalog, packageManifest.ModuleSources);
 
-                if (Module?.Length > 0 && !InstallPlatformParam)
+                if (Module?.Length > 0 && !PlatformParameter)
                 {
                     foreach (var module in ParseModuleParameter(Module))
                     {
@@ -88,13 +88,13 @@ namespace VirtoCommerce.Build
                         }
                     }
                 }
-                else if (!InstallPlatformParam && !packageManifest.Modules.Any())
+                else if (!PlatformParameter && !packageManifest.Modules.Any())
                 {
                     Logger.Info("Add group: commerce");
                     var commerceModules = externalModuleCatalog.Modules.OfType<ManifestModuleInfo>().Where(m => m.Groups.Contains("commerce")).Select(m => new ModuleItem(m.Id, m.Version.ToString()));
                     packageManifest.Modules.AddRange(commerceModules);
                 }
-                else if (InstallPlatformParam)
+                else if (PlatformParameter)
                 {
                     var platformRelease = await GithubManager.GetPlatformRelease(GitHubToken, VersionToInstall);
                     packageManifest.PlatformVersion = platformRelease.TagName;
@@ -291,19 +291,23 @@ namespace VirtoCommerce.Build
                 var platformRelease = await GithubManager.GetPlatformRelease(GitHubToken, VersionToInstall);
                 packageManifest.PlatformVersion = platformRelease.TagName;
                 packageManifest.PlatformAssetUrl = platformRelease.Assets.First().BrowserDownloadUrl;
-                var localModuleCatalog = LocalModuleCatalog.GetCatalog(GetDiscoveryPath(), ProbingPath);
-                var externalModuleCatalog = ExtModuleCatalog.GetCatalog(GitHubToken, localModuleCatalog, packageManifest.ModuleSources);
 
-                foreach (var module in packageManifest.Modules)
+                if (!PlatformParameter)
                 {
-                    var externalModule = externalModuleCatalog.Modules.OfType<ManifestModuleInfo>().Where(m => m.Id == module.Id).FirstOrDefault(m => m.Ref.Contains("github.com"));
+                    var localModuleCatalog = LocalModuleCatalog.GetCatalog(GetDiscoveryPath(), ProbingPath);
+                    var externalModuleCatalog = ExtModuleCatalog.GetCatalog(GitHubToken, localModuleCatalog, packageManifest.ModuleSources);
 
-                    if (externalModule == null)
+                    foreach (var module in packageManifest.Modules)
                     {
-                        ControlFlow.Fail($"No module {module.Id} found");
-                    }
+                        var externalModule = externalModuleCatalog.Modules.OfType<ManifestModuleInfo>().Where(m => m.Id == module.Id).FirstOrDefault(m => m.Ref.Contains("github.com"));
 
-                    module.Version = externalModule.Version.ToString();
+                        if (externalModule == null)
+                        {
+                            ControlFlow.Fail($"No module {module.Id} found");
+                        }
+
+                        module.Version = externalModule.Version.ToString();
+                    }
                 }
 
                 PackageManager.ToFile(packageManifest);
