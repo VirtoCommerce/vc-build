80d45e684244f2e17c321694bfe93a4087c85e9d
Alexander Morogov <asmorogov@gmail.com>
2021-12-15 16:20:16 +0200

    VCI-237: Add missing file

---
+++ src/VirtoCommerce.Build/Utils/NukeParameters.cs
@@ -0,0 +1,12 @@
+using System;
+using Newtonsoft.Json;
+
+namespace VirtoCommerce.Build.Utils
+{
+    public class NukeParameters
+    {
+        [JsonProperty("$schema")]
+        public string Schema { get; set; } = "./build.schema.json";
+        public string Solution { get; set; }
+    }
+}
