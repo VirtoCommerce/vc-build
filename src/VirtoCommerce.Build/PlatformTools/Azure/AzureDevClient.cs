45d6d3ae86759fa9903e29a2524559323bc20085
Alexandr Morogov <42555001+krankenbro@users.noreply.github.com>
2022-11-28 16:07:13 +0100

    VCI-480: Fix possible null reference exception in AzureDevClient (#69)

--- src/VirtoCommerce.Build/PlatformTools/Azure/AzureDevClient.cs
+++ src/VirtoCommerce.Build/PlatformTools/Azure/AzureDevClient.cs
@@ -1,38 +1,40 @@
 using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.TeamFoundation.Build.WebApi;
 using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.WebApi;
-using System.Threading.Tasks;
-using System.Linq;
-using System.IO;
+using Nuke.Common;
 
-namespace PlatformTools.Azure
+namespace PlatformTools.Azure;
+
+public class AzureDevClient
 {
-    public class AzureDevClient
+    private readonly VssConnection _connection;
+
+    public AzureDevClient(string orgName, string token)
     {
-        private readonly VssConnection _connection;
+        _connection = new VssConnection(new Uri(orgName), new VssBasicCredential(string.Empty, token));
+    }
 
-        public AzureDevClient(string orgName, string token)
-        {
-            _connection = new VssConnection(new Uri(orgName), new VssBasicCredential(string.Empty, token));
-        }
+    public async Task<Uri> GetArtifactUrl(Guid project, string branch, string definitionName)
+    {
+        var client = await _connection.GetClientAsync<BuildHttpClient>();
+        var build = await client.GetLatestBuildAsync(project, definitionName, branch);
+        var artifacts = await client.GetArtifactsAsync(project, build.Id);
+        Assert.NotEmpty(artifacts,
+            $"No builds found for the project id: {project.ToString()}{Environment.NewLine}branch: {branch}{Environment.NewLine}definition: {definitionName}");
+        var result = artifacts.FirstOrDefault()?.Resource.DownloadUrl;
 
-        public async Task<Uri> GetArtifactUrl(Guid project, string branch, string definitionName)
-        {
-            var client = await _connection.GetClientAsync<BuildHttpClient>();
-            var build = await client.GetLatestBuildAsync(project, definitionName, branch);
-            var artifacts = await client.GetArtifactsAsync(project, build.Id);
-            var result = artifacts.FirstOrDefault().Resource.DownloadUrl;
+        return new Uri(result ?? string.Empty);
+    }
 
-            return new Uri(result);
-        }
-
-        public async Task<Stream> GetArtifactStream(Guid project, string branch, string definitionName)
-        {
-            var client = await _connection.GetClientAsync<BuildHttpClient>();
-            var build = await client.GetLatestBuildAsync(project, definitionName, branch);
-            var result = await client.GetArtifactContentZipAsync(project, build.Id, "backend");
-            return result;
-        }
+    public async Task<Stream> GetArtifactStream(Guid project, string branch, string definitionName)
+    {
+        var client = await _connection.GetClientAsync<BuildHttpClient>();
+        var build = await client.GetLatestBuildAsync(project, definitionName, branch);
+        var result = await client.GetArtifactContentZipAsync(project, build.Id, "backend");
+        return result;
     }
 }
