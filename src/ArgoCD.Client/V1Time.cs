7fd24a6354ff8121dbc9f5c07a346a8f4fa6168c
Alexander Morogov <asmorogov@gmail.com>
2023-09-08 16:03:47 +0200

    VCI-664: Remove redundant files

--- src/ArgoCD.Client/V1Time.cs
+++
@@ -1,9 +0,0 @@
-using System;
-namespace ArgoCD.Client.Models
-{
-    public partial class V1Time
-    {
-        public static implicit operator V1Time(DateTime dateTime) => new V1Time(seconds: new DateTimeOffset(dateTime).ToUnixTimeSeconds().ToString());
-    }
-}
-
