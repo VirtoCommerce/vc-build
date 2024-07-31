bed35a8800357679b1aee3f4fa19888f1843dc6b
Alexander Morogov <42555001+krankenbro@users.noreply.github.com>
2024-07-31 08:37:28 +0200

    VCI-904: GlobalModuleIgnoreFileUrl can be a local file (#142)

--- src/VirtoCommerce.Build/Build.cs
+++ src/VirtoCommerce.Build/Build.cs
@@ -1252,9 +1252,22 @@ internal partial class Build : NukeBuild
             const string moduleIgnoreUrlTemplate = "https://raw.githubusercontent.com/VirtoCommerce/vc-platform/{0}/module.ignore";
             if(string.IsNullOrEmpty(GlobalModuleIgnoreFileUrl))
             {
-                GlobalModuleIgnoreFileUrl = string.Format(moduleIgnoreUrlTemplate, ModuleManifest?.PlatformVersion ?? "dev");
+                Version.TryParse(ModuleManifest.PlatformVersion, out var platformVersion);
+
+                const int MajorMinorPatch = 3;
+                GlobalModuleIgnoreFileUrl = string.Format(moduleIgnoreUrlTemplate, platformVersion?.ToString(MajorMinorPatch) ?? "dev");
             }
-            var ignoredFiles = HttpTasks.HttpDownloadString(GlobalModuleIgnoreFileUrl).SplitLineBreaks();
+
+            string[] ignoredFiles;
+            if (GlobalModuleIgnoreFileUrl.StartsWith("http"))
+            {
+                ignoredFiles = HttpTasks.HttpDownloadString(GlobalModuleIgnoreFileUrl).SplitLineBreaks();
+            }
+            else
+            {
+                ignoredFiles = File.ReadAllLines(GlobalModuleIgnoreFileUrl);
+            }
+
 
             if (ModuleIgnoreFile.FileExists())
             {
