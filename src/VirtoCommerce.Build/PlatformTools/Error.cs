b28f0fe7664f8bf72fcf214cfa8cc6dad32d2ddc
krankenbro <42555001+krankenbro@users.noreply.github.com>
2025-08-01 08:07:29 +0200

    VCI-1116: Add dependency versions validation (#167)
    Co-authored-by: Artem Dudarev <artem@virtoworks.com>

---
+++ src/VirtoCommerce.Build/PlatformTools/Error.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using PlatformTools;
+using VirtoCommerce.Platform.Core.Modularity;
+
+namespace VirtoCommerce.Build;
+
+internal class Error(string messageTemplate, params object[] propertyValues)
+{
+    public string MessageTemplate { get; } = messageTemplate;
+    public object[] PropertyValues { get; } = propertyValues;
+
+    public static Error MissingManifestDependency(string moduleId, string version)
+    {
+        return new Error("Missing dependency in module.manifest. Module {ModuleId}, version: {Version}",
+            moduleId, version);
+    }
+
+    public static Error PlatformMultipleVersions(IList<string> versions, IList<string> projects)
+    {
+        return new Error("Platform has multiple versions {Versions} in projects {Projects}",
+            versions, projects);
+    }
+
+    public static Error ModuleMultipleVersions(string moduleId, IList<string> versions, IList<string> projects)
+    {
+        return new Error("Module {ModuleId} has multiple versions {Versions} in projects {Projects}",
+            moduleId, versions, projects);
+    }
+
+    public static Error PlatformVersionMismatch(string manifestVersion, PackageItem package)
+    {
+        return new Error(
+            "Platform version mismatch. Manifest version: {ManifestVersion}, package name: {PackageName}, package version: {PackageVersion}, project name: {ProjectName}",
+            manifestVersion, package.Name, package.Version, package.ProjectName);
+    }
+
+    public static Error ModuleVersionMismatch(ManifestDependency dependency, PackageItem package)
+    {
+        return new Error(
+            "Module version mismatch. Module: {DependencyId}, manifest version: {DependencyVersion}, package name: {PackageName}, package version: {PackageVersion}, project name: {ProjectName}",
+            dependency.Id, dependency.Version, package.Name, package.Version, package.ProjectName);
+    }
+}
