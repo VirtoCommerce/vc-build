89fe3086e52b6b6a355381afcbab50a7deddd04f
krankenbro <42555001+krankenbro@users.noreply.github.com>
2025-10-09 07:02:46 +0200

    VCI-1176: Fix fail in install target when there  is no GithubReleases… (#175)

--- src/VirtoCommerce.Build/PlatformTools/GithubReleases.cs
+++ src/VirtoCommerce.Build/PlatformTools/GithubReleases.cs
@@ -3,11 +3,11 @@ using PlatformTools.Modules;
 
 namespace VirtoCommerce.Build.PlatformTools
 {
-    public class GithubReleases: ModuleSource
+    public class GithubReleases : ModuleSource
     {
         public override string Name { get; set; } = "GithubReleases";
-        public List<string> ModuleSources { get; set; }
-        public List<ModuleItem> Modules { get; set; }
+        public List<string> ModuleSources { get; set; } = [];
+        public List<ModuleItem> Modules { get; set; } = [];
     }
 }
 
