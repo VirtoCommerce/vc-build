db5f2b0b3c6a27aaf95fe86a7ecddbd73b36eec2
Alexander Morogov <asmorogov@gmail.com>
2026-03-12 10:11:14 +0100

    VCI-1202: Fix for module sources names

--- src/VirtoCommerce.Build/PlatformTools/Modules/Github/GithubPrivateRepos.cs
+++ src/VirtoCommerce.Build/PlatformTools/Modules/Github/GithubPrivateRepos.cs
@@ -4,7 +4,8 @@ namespace PlatformTools.Modules.Github
 {
     internal class GithubPrivateRepos : ModuleSource
     {
-        public override string Name { get; set; } = "GithubPrivateRepos";
+        public const string SourceName = nameof(GithubPrivateRepos);
+        public override string Name { get; set; } = SourceName;
         public string Owner { get; set; }
         public List<ModuleItem> Modules { get; set; }
     }
--- src/VirtoCommerce.Build/PlatformTools/Modules/LocalModules/LocalModuleSource.cs
+++ src/VirtoCommerce.Build/PlatformTools/Modules/LocalModules/LocalModuleSource.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 namespace PlatformTools.Modules.LocalModules;
 internal class LocalModuleSource : ModuleSource
 {
-    public override string Name { get; set; } = "Local";
+    public const string SourceName = "Local";
+    public override string Name { get; set; } = SourceName;
     public List<LocalModuleItem> Modules { get; set; }
 }
